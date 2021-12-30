933a0b0a8fd7d25408c70f34abd6a201897aa335
F1ks3r <f1ks3r@yandex.ru>
2021-12-30 16:25:00 +0300

    save

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import TestComponent from './Test.vue'
 import store from './store.js'
 
 const Test = {
-    install(Vue, args = {}) {
+    install(Vue, options) {
         if (this.installed) { return }
 
         this.installed = true
 
         Vue.component('test', TestComponent)
+
+        console.log('install options', options)
 
         const Test = params => {
             store.dispatch('test', params)
